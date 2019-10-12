f2066f271e40a9b717ce95ddcd821bf522b59098
John Erick Madduma <madduma.johnerick@gmail.com>
2019-10-12 13:10:29 +0800

    test hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi there</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <br />
-    <Link to="/about/">Go to about</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [name, setName] = useState("")
+
+  useEffect(() => {
+    setInterval(() => {
+      setName("Test")
+    }, 3000)
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi there, {name}</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      {/* <button onClick={() => setName("Test")}>Set</button> */}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+      <br />
+      <Link to="/about/">Go to about</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
